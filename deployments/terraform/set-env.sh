df268d393ba29917faa216225a72a56e5c05c91d
Emilbekdevops <emilbeksb@gmail.com>
2020-09-11 18:47:22 +0000

    nes set-env added

--- deployments/terraform/set-env.sh
+++ deployments/terraform/set-env.sh
@@ -1,12 +1,67 @@
-#!/usr/bin/env bash
-if [ -z "$GIT_TOKEN" ]
-then
-  echo """
-  Script needs github token to get private repos.
-  Github <GIT_TOKEN> not found."""
-  return 1 
+#!/bin/bash
+
+DIR=$(pwd)
+DATAFILE="$DIR/$1"
+#
+# FuchiCorp common script to set up Google terraform environment variables
+# these all variables should be created on your config file before you run script.
+# <ENVIRONMENT> <BUCKET> <DEPLOYMENT> <PROJECT> <CREDENTIALS>
+
+if [ ! -f "$DATAFILE" ]; then
+  echo "setenv: Configuration file not found: $DATAFILE"
+  return 1
 fi
-wget --quiet "--header=Authorization: token $GIT_TOKEN" "https://raw.githubusercontent.com/fuchicorp/common_scripts/master/set-environments/terraform/google-set-env.sh" \
-  -O set-env >/dev/null
-source 'set-env' "$@"
-rm -rf "set-env"
+BUCKET=$(sed -nr 's/^google_bucket_name\s*=\s*"([^"]*)".*$/\1/p'             "$DATAFILE")
+PROJECT=$(sed -nr 's/^google_project_id\s*=\s*"([^"]*)".*$/\1/p'             "$DATAFILE")
+ENVIRONMENT=$(sed -nr 's/^deployment_environment\s*=\s*"([^"]*)".*$/\1/p'    "$DATAFILE")
+DEPLOYMENT=$(sed -nr 's/^deployment_name\s*=\s*"([^"]*)".*$/\1/p'            "$DATAFILE")
+CREDENTIALS=$(sed -nr 's/^credentials\s*=\s*"([^"]*)".*$/\1/p'               "$DATAFILE") 
+
+if [ -z "$ENVIRONMENT" ]
+then
+    echo "setenv: 'deployment_environment' variable not set in configuration file."
+    return 1
+fi
+
+if [ -z "$BUCKET" ]
+then
+  echo "setenv: 'google_bucket_name' variable not set in configuration file."
+  return 1
+fi
+
+if [ -z "$PROJECT" ]
+then
+    echo "setenv: 'google_project_id' variable not set in configuration file."
+    return 1
+fi
+
+if [ -z "$CREDENTIALS" ]
+then
+    echo "setenv: 'credentials' file not set in configuration file."
+    return 1
+fi
+
+if [ -z "$DEPLOYMENT" ]
+then
+    echo "setenv: 'deployment_name' variable not set in configuration file."
+    return 1
+fi
+
+cat << EOF > "$DIR/backend.tf"
+terraform {
+  backend "gcs" {
+    bucket  = "${BUCKET}"
+    prefix  = "${ENVIRONMENT}/${DEPLOYMENT}"
+    project = "${PROJECT}"
+  }
+}
+EOF
+cat "$DIR/backend.tf"
+
+GOOGLE_APPLICATION_CREDENTIALS="${DIR}/${CREDENTIALS}"
+export GOOGLE_APPLICATION_CREDENTIALS
+export DATAFILE
+/bin/rm -rf "$DIR/.terraform" 2>/dev/null
+/bin/rm -rf "$PWD/common_configuration.tfvars" 2>/dev/null
+echo "setenv: Initializing terraform"
+terraform init #> /dev/null
