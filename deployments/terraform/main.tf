2dfe282a9889f7fe1d4fc5b32dd1593d57859bc4
Emilbekdevops <63379202+Emilbekdevops@users.noreply.github.com>
2020-09-14 01:20:59 -0500

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,21 +1,29 @@
 module "academy-deploy" {
   source  = "fuchicorp/chart/helm"
+
   deployment_name        = "hello-world"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
   deployment_path        = "hello-world"
-  template_custom_vars  = {
-    deployment_image = "${var.deployment_image}"
-   }
+
+  template_custom_vars  = {     
+    deployment_image     = "${var.deployment_image}"
+  }
 }
+
+
 output "application_endpoint" {
     value = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
 }
+
+
+
 variable  "deployment_image" {
-    default = "docker.emilbek.com/hello-world-job-dev-feature:d0d2dfe"
+    default = "docker.emilbek.com/hello-world-job-dev-feature:565b43e"
 }
+
 variable "deployment_environment" {
-    default = "qa"
+    default = "dev"
 }
 
 variable "deployment_endpoint" {
@@ -28,6 +36,7 @@ variable "deployment_endpoint" {
         stage = "stage.hello"
   }
 }
+
 variable "google_domain_name" {
     default = "emilbek.com"
 }
