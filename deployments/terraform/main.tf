f3eaad02b88b3bb2f5ade4618e26c3f9b539bddd
Emilbekdevops <emilbeksb@gmail.com>
2020-09-15 21:03:53 +0000

    added some changes

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -5,7 +5,7 @@ module "academy-deploy" {
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
   deployment_path        = "hello-world"
   template_custom_vars  = {
-    deployment_image= "${var.deployment_image}"
+    deployment_image = "${var.deployment_image}"
    }
 }
 output "application_endpoint" {
