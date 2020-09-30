76c45b0da4c37a5d68dbbf7ca2f23bb569874d8b
emilbekdevops <63379202+emilbekdevops@users.noreply.github.com>
2020-09-29 21:30:55 -0500

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,31 +1,22 @@
-module "academy-deploy" {
-  source  = "fuchicorp/chart/helm"
-
-  deployment_name        = "hello-world"
-  deployment_environment = "${var.deployment_environment}"
-  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
-  deployment_path        = "hello-world"
-
-  template_custom_vars  = {     
-    deployment_image     = "${var.deployment_image}"
-  }
-}
-
-
+ module "academy-deploy" {
+   source  = "fuchicorp/chart/helm"
+   deployment_name        = "hello-world"
+   deployment_environment = "${var.deployment_environment}"
+   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+   deployment_path        = "hello-world"
+   template_custom_vars  = {     
+     deployment_image     = "${var.deployment_image}"
+   }
+ }
 output "application_endpoint" {
     value = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
 }
-
-
-
 variable  "deployment_image" {
-    default = "docker.emilbek.com/hello-world-job-dev-feature:565b43e"
+    default = "docker.emilbek.com/hello-world-job-stage:56c11ad"
 }
-
 variable "deployment_environment" {
-    default = "dev"
+    default = "stage"
 }
-
 variable "deployment_endpoint" {
     type = "map"
      default = {
@@ -36,10 +27,10 @@ variable "deployment_endpoint" {
         stage = "stage.hello"
   }
 }
-
 variable "google_domain_name" {
     default = "emilbek.com"
 }
+
 variable "google_bucket_name" {
-    default = "fuchicorp-emil-343"
+    default = "emilsbucket"
 }
